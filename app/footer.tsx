2f96d3637f67ce4376a771815aad6e8cdd613358
utnim2 <mintugogoi567@gmail.com>
2025-08-13 22:19:55 +0530

    feat: completed the blog site

---
+++ app/footer.tsx
@@ -0,0 +1,20 @@
+import { A } from "./(post)/components/a";
+
+export function Footer() {
+  return (
+    <footer className="p-6 pt-3 pb-6 flex text-xs text-center mt-3 dark:text-gray-400 text-gray-500 font-mono">
+      <div className="grow text-left">
+        Mintu Gogoi (
+        <A target="_blank" href="https://x.com/Min2_gg">
+          @Min2__gg
+        </A>
+        )
+      </div>
+      <div>
+        <A target="_blank" href="https://github.com/Gmin2/blog">
+          Source
+        </A>
+      </div>
+    </footer>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,64 @@
+import "./globals.css";
+
+import { Inter } from "next/font/google";
+import { themeEffect } from "./theme-effect";
+import { Analytics } from "./analytics";
+import { Header } from "./header";
+import { Footer } from "./footer";
+import { doge } from "./doge";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Gmin",
+  description: "Hi I am Mintu Gogoi, welcome to my blog.",
+  openGraph: {
+    title: "Mintu's blog",
+    description: "Hi I am Mintu Gogoi, welcome to my blog.",
+    url: "https://gmin.blog.in",
+    siteName: "Mintu's blog",
+    images: ["/opengraph-image"],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@Min2__gg",
+    creator: "@Min2__gg",
+  },
+  metadataBase: new URL("https://gmin.blog.in"),
+};
+
+export const viewport = {
+  themeColor: "transparent",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className={`${inter.className} antialiased`}
+      suppressHydrationWarning={true}
+    >
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(${themeEffect.toString()})();(${doge.toString()})();`,
+          }}
+        />
+      </head>
+
+      <body className="dark:text-gray-100 max-w-2xl m-auto">
+        <main className="p-6 pt-3 md:pt-6 min-h-screen">
+          <Header />
+          {children}
+        </main>
+
+        <Footer />
+        <Analytics />
+      </body>
+    </html>
+  );
+}
